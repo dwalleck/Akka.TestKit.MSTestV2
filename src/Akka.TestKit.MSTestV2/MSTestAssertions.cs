0d52151e9421e6dd05b3a6249a53165b3537d75d
Daryl Walleck <dwalleck@gmail.com>
2017-09-10 16:45:04 -0500

    Initial add of MSTest support for Akka.NET

---
+++ src/Akka.TestKit.MSTestV2/MSTestAssertions.cs
@@ -0,0 +1,51 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MSTestAssertions.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2016 Typesafe Inc. <http://www.typesafe.com>
+//     Copyright (C) 2013-2017 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Akka.TestKit.MSTestV2
+{
+    /// <summary>
+    /// Assertions for the MSTest Framework
+    /// </summary>
+    class MSTestAssertions : ITestKitAssertions
+    {
+        public void Fail(string format = "", params object[] args)
+        {
+            Assert.Fail(format, args);
+        }
+
+        public void AssertTrue(bool condition, string format = "", params object[] args)
+        {
+            Assert.IsTrue(condition, format, args);
+        }
+
+        public void AssertFalse(bool condition, string format = "", params object[] args)
+        {
+
+            Assert.IsFalse(condition, format, args);
+        }
+
+        public void AssertEqual<T>(T expected, T actual, string format = "", params object[] args)
+        {
+            Assert.AreEqual(expected, actual, format, args);
+        }
+
+        public void AssertEqual<T>(T expected, T actual, Func<T, T, bool> comparer, string format = "", params object[] args)
+        {
+            if (!comparer(expected, actual))
+                throw new AssertFailedException(string.Format("Assert.AreEqual failed. Expected [{0}]. Actual [{1}]. {2}", FormatValue(expected), FormatValue(actual), string.Format(format, args)));
+        }
+
+        private static string FormatValue<T>(T expected)
+        {
+            return ReferenceEquals(expected, null) ? "null" : expected.ToString();
+        }
+
+    }
+}
---
+++ src/Akka.TestKit.MSTestV2/TestKit.cs
@@ -0,0 +1,58 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestKit.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2016 Typesafe Inc. <http://www.typesafe.com>
+//     Copyright (C) 2013-2017 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace Akka.TestKit.MSTestV2
+{
+    class TestKit : TestKitBase
+    {
+        private static readonly MSTestAssertions _assertions = new MSTestAssertions();
+
+        /// <summary>
+        /// Create a new instance of the <see cref="TestKit"/> for the test class.
+        /// If no <paramref name="system"/> is passed in, a new system 
+        /// with <see cref="DefaultConfig"/> will be created.
+        /// </summary>
+        /// <param name="system">Optional: The actor system.</param>
+        public TestKit(ActorSystem system = null)
+            : base(_assertions, system)
+        {
+            //Intentionally left blank
+        }
+
+        /// <summary>
+        /// Create a new instance of the <see cref="TestKit"/> for the test class.
+        /// A new system with the specified configuration will be created.
+        /// </summary>
+        /// <param name="config">The configuration to use for the system.</param>
+        /// <param name="actorSystemName">Optional: the name of the system. Default: "test"</param>
+        public TestKit(Config config, string actorSystemName = null)
+            : base(_assertions, config, actorSystemName)
+        {
+            //Intentionally left blank
+        }
+
+
+        /// <summary>
+        /// Create a new instance of the <see cref="TestKit"/> for the test class.
+        /// A new system with the specified configuration will be created.
+        /// </summary>
+        /// <param name="config">The configuration to use for the system.</param>
+        public TestKit(string config) : base(_assertions, ConfigurationFactory.ParseString(config))
+        {
+            //Intentionally left blank
+        }
+
+        public new static Config DefaultConfig { get { return TestKitBase.DefaultConfig; } }
+        public new static Config FullDebugConfig { get { return TestKitBase.FullDebugConfig; } }
+
+        protected static MSTestAssertions Assertions { get { return _assertions; } }
+
+    }
+}
